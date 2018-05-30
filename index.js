c35ccebc814d1aa18e40ee9f372550e535d04e1e
fodon11 <francisco.odon11@gmail.com>
2018-05-30 19:16:17 -0400

    solved problem 3

--- index.js
+++ index.js
@@ -27,23 +27,57 @@ console.log(p)
 }
 
 // What is the largest prime factor of the number 600851475143 ?
-function findFactors(num){
-	let prime;
+function findPrimeFactors(num){
+	factor = [];
+	factor2 = [];
+	// findFactor(num, factor);
+	checkIfPrime(num, factor)
+	console.log(factor)
+	checkIfPrime(factor[1], factor)
+	console.log(factor);
+  
+	checkIfPrime(factor[2], factor)
+	console.log(factor)
+  
+	checkIfPrime(factor[3], factor)
+	console.log(factor)
+	// findFactor(factor[1], factor2);
+	
+	// console.log(factor2);
+  
+	// checkIfPrime(factor[0]);
+	// checkIfPrime(factor[1], factor);
+	// console.log(factor);
+	// findFactor(factor2[0], factor2);
+	// factor2.push(factor2[0]/factor2[1]);
+}
+  
+  
+function findFactor(num, array){
+	let o;
 	for (var i=2; i<num; i++){
-            if (num % i == 0){
-				let ifNotPrime = null;
-				for (var e=2; e<i-1; e++){
-					if(i % e ==0){
-						ifNotPrime = i;
-                    }
-                }
-			if (ifNotPrime === null){
-// 			console.log(i)
-			prime = i
-// 			console.log(prime)
-// 			prime.push(i)
-            }
-            }
-        }
-console.log(prime)
+		if (num % i === 0){
+		array.pop();
+		array.push(i);
+		o = i
+		break;
+		}
+	}
+	array.push(num/o)
+}
+  
+function checkIfPrime(num, array, array2){
+	let number = [];
+	for (var i=2; i<num; i++){
+	  if (num % i === 0){
+		number.push(i);
+		break;
+	  }
+	}
+	if (number.length > 0){
+	  console.log(num + ' is not prime');
+	  findFactor(num, array)
+	} else if (number.length == 0){
+	  console.log(num + ' is a prime number');
+	}
 }
